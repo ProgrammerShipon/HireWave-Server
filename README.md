162d0fbdea2f929d71d3c8c9fcb39bcb3c36faf6
ProgrammerShipon <msshipon234@gmail.com>
2023-08-13 13:34:29 +0600

    development server added

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # HireWave-Server
+
+## APIs
+
+<article>
+<p>Testing Route :
+<a> http://localhost:3030/ </a>
+ </p>
+</article>
+
+## Using Package
+
+- morgan ( Client Calling Route Show )
+
+## Collaborators
+
+### Shipon Hossen Raju
--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = process.env.PORT || 3030;
 
 // middleware
+app.use(morgan("dev"));
+app.use(express.json())
+
+// middleware Function
+const isLogin = (req, res, next) => {
+   const login = true;
+   if (login) {
+      next();
+   } else {
+      return res.status(401).send({message: 'please login'});
+   }
+}
 
 // general apis
 app.get('/', (req, res) => res.status(200).send('Assalamualaikum'))
+
+// Route or apis
 
 
 // Running Server
