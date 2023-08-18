14c2e4b24d4d5b57238a8ca7467139b2433b30dd
radifkhanrafin <meherafkabir@gmail.com>
2023-08-18 15:41:41 +0600

    create a new brance

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3030;
+const express = require("express");
+const { ObjectId } = require("mongodb");
+const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
+// express app initialization
+const app = express();
+app.use(express.json());
 
-// middleware
 
-// general apis
-app.get('/', (req, res) => res.status(200).send('Assalamualaikum'))
+// Create  schema 
+// Create Collection
+// database connection with mongoose
+const connectDB = async () => {
+   try {
+      await mongoose.connect('mongodb+srv://hiqmah-607:3bSe7cpTWxq6iTOL@cluster0.v0qnf3o.mongodb.net/')
+      console.log('DB connect success')
+   } catch (error) {
+      console.log('DB Not Connect', error)
+   }
+}
 
 
-// Running Server
-app.listen(port, () => {
-   console.log(`Server Running - http://localhost:${port}`);
+app.get('/', (req, res) => {
+   res.send("sonar bangladesh")
 })
+
+app.listen(port, async () => {
+   console.log(`app listening at port ${port}`);
+   await connectDB()
+});
