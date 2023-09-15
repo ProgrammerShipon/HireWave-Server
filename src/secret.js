58a6fa9c97db2281355ad2792cf27dfdb873e26d
Shipon Hossen Raju <msshipon234@gmail.com>
2023-09-15 14:54:28 +0600

    bad athentication

--- src/secret.js
+++ src/secret.js
@@ -7,9 +7,6 @@ const PORT = process.env.SERVER_PORT || 3031;
 const mongodbURL = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.v0qnf3o.mongodb.net/`;
 // const mongodbURL = process.env.MONGODB_URL;
 
-
-
-
 // JWT secrete token
 const jwtSecret = process.env.JWT_ACCESS_TOKEN;
 
